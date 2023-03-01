6f06476f65da03569b5fa38f503034081b6f061d
wangjunchen <wangjunchen2017@outlook.com>
2023-03-01 15:33:18 +0800

    修复查看商品评论时商品被删除的bug

--- power-business/product-service/src/main/java/com/powernode/service/impl/ProdCommServiceImpl.java
+++ power-business/product-service/src/main/java/com/powernode/service/impl/ProdCommServiceImpl.java
@@ -19,6 +19,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -38,7 +39,7 @@ public class ProdCommServiceImpl extends ServiceImpl<ProdCommMapper, ProdComm> i
         String prodName = prodComm.getProdName();
         //判断商品名称是否有值
         List<Long> prodIdList = null;
-        List<Prod> prodList = null;
+        List<Prod> prodList;
         if (StringUtils.hasText(prodName)) {
             //有值：根据商品名称模糊查询商品集合
             prodList = prodMapper.selectList(new LambdaQueryWrapper<Prod>()
@@ -55,7 +56,7 @@ public class ProdCommServiceImpl extends ServiceImpl<ProdCommMapper, ProdComm> i
         //分页查询商品评论
         page = prodCommMapper.selectPage(page, new LambdaQueryWrapper<ProdComm>()
                 .eq(ObjectUtil.isNotEmpty(prodComm.getStatus()), ProdComm::getStatus, prodComm.getStatus())
-                .in(CollectionUtil.isNotEmpty(prodIdList) && prodIdList.size() != 0, ProdComm::getProdId, prodIdList)
+                .in(CollectionUtil.isNotEmpty(prodIdList) && Objects.requireNonNull(prodIdList).size() != 0, ProdComm::getProdId, prodIdList)
                 .orderByDesc(ProdComm::getRecTime)
         );
         //获取评论记录
@@ -66,12 +67,16 @@ public class ProdCommServiceImpl extends ServiceImpl<ProdCommMapper, ProdComm> i
             List<Long> prodIds = prodCommList.stream().map(ProdComm::getProdId).collect(Collectors.toList());
             //根据商品id集合查询商品集合
             List<Prod> prods = prodMapper.selectBatchIds(prodIds);
+            //获取仍旧存在的商品id集合
+            List<Long> existProdIdList = prods.stream().map(Prod::getProdId).collect(Collectors.toList());
             //循环评论记录
             prodCommList.forEach(prodComm1 -> {
-                //从商品集合中过滤出与当前评论中的商品一致的商品对象
-                Prod prod1 = prods.stream().filter(prod -> prod.getProdId().equals(prodComm1.getProdId())).collect(Collectors.toList()).get(0);
-                //当评论存在，但是商品被删除的时候会出现异常，即通过评论中的商品id查询对应的商品名称时无法查询到
-                prodComm1.setProdName(prod1.getProdName());
+                if (existProdIdList.contains(prodComm1.getProdId())) {
+                    //从商品集合中过滤出与当前评论中的商品一致的商品对象
+                    //当评论存在，但是评论中对应prod_id的商品信息已经被删除，此时会出现异常，因此需要先判断商品是否还存在
+                    Prod prod1 = prods.stream().filter(prod -> prod.getProdId().equals(prodComm1.getProdId())).collect(Collectors.toList()).get(0);
+                    prodComm1.setProdName(prod1.getProdName());
+                }
             });
         }
         return page;
